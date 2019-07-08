ebdab4c63040a2cbfbe5351fd19012b65a13e81a
yateeshbant <yateeshbant2@gmail.com>
2019-07-08 01:02:39 -0700

    final

--- Ga.py
+++ Ga.py
@@ -1,5 +1,5 @@
 import numpy 
-import random 
+import random ,pickle
 class Model():
     def __init__(self):
         self.size=[]
@@ -15,9 +15,9 @@ class Population():
     def initialize(self,sizes):
         self.genes=[]
         for i in sizes.size:
-            self.genes.append(numpy.random.rand(i[0],i[1]))
-        
-        
+            self.genes.append(numpy.random.uniform(low=-1, high=1, size=(i[0],i[1])))
+                     
+                
 
 class Ga():
     def __init__(self,popsize,model):
@@ -27,10 +27,33 @@ class Ga():
         self.popsize=popsize
         self.newbreed=[]
         self.initpopulation(popsize,model)
+        self.max_mut=0.05
+        self.min_mut=0.0005
+        self.max_rev=500
+
+    def save(self,name="data.sv"):
+        savedata={}
+        savedata["model"]=self.model
+        savedata["selection"]=self.selection
+        savedata["mutation"]=self.mutation
+        savedata["popsize"]=self.popsize
+        savedata["newbreed"]=self.newbreed
+        with open(name , 'wb') as f:
+            pickle.dump(savedata, f, pickle.HIGHEST_PROTOCOL)
+        
+
+    def load(self,name="data.sv"):
+        with open(name , 'rb') as f:
+            loaded= pickle.load(f)
+        self.model=loaded["model"]
+        self.selection=loaded["selection"]
+
+        self.mutation=loaded["mutation"]
+        self.popsize=loaded["popsize"]
+        self.newbreed=loaded["newbreed"]
 
     def initpopulation(self,popsize,modelsize):  #int model()
         self.population=[]
-        
         for i in range(popsize):
             x=Population(0)
             x.initialize(modelsize)
@@ -41,7 +64,8 @@ class Ga():
         self.b=[]
         for i in self.population:
             self.b.append(i.reward)
-        
+    
+ 
         self.b=sorted(range(len(self.b)),key=self.b.__getitem__)[0:self.selection]
          
 
@@ -51,8 +75,8 @@ class Ga():
 
     def cross(self,a,b):
         self.model=[]
+        gene=[]
         for x,y in zip(a.genes,b.genes):
-            gene=[]
             temp=numpy.empty((x.shape))
             temp.T[0:int(x.shape[1]/2)]=x.T[0:int(x.shape[1]/2)]
             temp.T[int(x.shape[1]/2):x.shape[1]]=y.T[int(x.shape[1]/2):x.shape[1]]
@@ -64,14 +88,13 @@ class Ga():
         a,b=temp.shape
         b-=1
         for i in range(a):
-            temp[i][random.randint(0,b)]+=self.mutation*random.randint(-1,1)
+            temp[i][random.randint(0,b)]+=self.mutation*random.uniform(-1,1)
         return temp
             
     def predict(self,popnum,theta):
         genes=self.population[popnum].genes
-         
         for i in genes:
-            theta=numpy.dot(theta,i)
+            theta=numpy.dot(theta,i)+numpy.dot(numpy.ones(theta.shape),i)
         return theta
     
     def run(self,x):
